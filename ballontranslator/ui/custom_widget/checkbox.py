dd42f8f943298a2aaede277de0b972cf4d1ab017
Kim,SangGu <peperu@outlook.com>
2026-06-12 20:15:48 +0900

    Preserve macOS format button width in mixed state

--- ballontranslator/ui/custom_widget/checkbox.py
+++ ballontranslator/ui/custom_widget/checkbox.py
@@ -5,16 +5,28 @@ from qtpy.QtCore import Qt
 from qtpy.QtGui import QMouseEvent
 
 class QFontChecker(QCheckBox):
+    BASE_MIN_WIDTH = 63 if sys.platform == 'darwin' else 0
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if sys.platform == 'darwin':
-            self.setStyleSheet("min-width: 45px")
+        if self.BASE_MIN_WIDTH:
+            self.setMinimumWidth(self.BASE_MIN_WIDTH)
+        self.resetStyleSheet()
+
+    def resetStyleSheet(self):
+        self.setStyleSheet("")
 
 class AlignmentChecker(QCheckBox):
+    BASE_MIN_WIDTH = 33 if sys.platform == 'darwin' else 0
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if sys.platform == 'darwin':
-            self.setStyleSheet("min-width: 15px")
+        if self.BASE_MIN_WIDTH:
+            self.setMinimumWidth(self.BASE_MIN_WIDTH)
+        self.resetStyleSheet()
+
+    def resetStyleSheet(self):
+        self.setStyleSheet("")
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
         if self.checkState() == Qt.CheckState.Checked:
