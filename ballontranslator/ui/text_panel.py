1ec71e881d4adc67ef01a24cef8422b1f0811817
Kim,SangGu <peperu@outlook.com>
2026-06-12 20:15:48 +0900

    Preserve macOS format button width in mixed state

--- ballontranslator/ui/text_panel.py
+++ ballontranslator/ui/text_panel.py
@@ -57,6 +57,17 @@ AlignmentChecker#AlignRightChecker::indicator:indeterminate {{
 """
 
 
+def set_checker_mixed_style(checker):
+    checker.setStyleSheet(mixed_checkbox_style())
+
+
+def reset_checker_style(checker):
+    if hasattr(checker, 'resetStyleSheet'):
+        checker.resetStyleSheet()
+    else:
+        checker.setStyleSheet("")
+
+
 COLOR_FIELDS = {'frgb', 'srgb', 'shadow_color', 'gradient_start_color', 'gradient_end_color'}
 
 
@@ -161,13 +172,13 @@ class AlignmentBtnGroup(QFrame):
             checker.blockSignals(True)
             checker.setTristate(True)
             checker.setCheckState(Qt.CheckState.PartiallyChecked)
-            checker.setStyleSheet(mixed_checkbox_style())
+            set_checker_mixed_style(checker)
             checker.blockSignals(False)
 
     def alignBtnPressed(self):
         for checker in self._checkers():
             checker.setTristate(False)
-            checker.setStyleSheet("")
+            reset_checker_style(checker)
         btn = self.sender()
         if btn == self.alignLeftChecker:
             self.alignLeftChecker.setChecked(True)
@@ -188,7 +199,7 @@ class AlignmentBtnGroup(QFrame):
     def setAlignment(self, alignment: int):
         for checker in self._checkers():
             checker.setTristate(False)
-            checker.setStyleSheet("")
+            reset_checker_style(checker)
         if alignment == 0:
             self.alignLeftChecker.setChecked(True)
             self.alignCenterChecker.setChecked(False)
@@ -227,14 +238,14 @@ class FormatGroupBtn(QFrame):
         btn.setTristate(True)
         btn.setCheckState(Qt.CheckState.PartiallyChecked)
         btn.setProperty('mixed', True)
-        btn.setStyleSheet(mixed_checkbox_style())
+        set_checker_mixed_style(btn)
         btn.blockSignals(False)
 
     def clearMixed(self):
         for btn in (self.boldBtn, self.italicBtn, self.underlineBtn):
             btn.blockSignals(True)
             btn.setProperty('mixed', False)
-            btn.setStyleSheet("")
+            reset_checker_style(btn)
             btn.setTristate(False)
             btn.blockSignals(False)
 
@@ -799,7 +810,7 @@ class FontFormatPanel(Widget):
         self.lineSpacingBox.setValue(font_format.line_spacing)
         self.letterSpacingBox.setValue(font_format.letter_spacing)
         self.verticalChecker.setTristate(False)
-        self.verticalChecker.setStyleSheet("")
+        reset_checker_style(self.verticalChecker)
         self.verticalChecker.setChecked(font_format.vertical)
         weight = font_format.font_weight if font_format.font_weight is not None else (700 if font_format.bold else 400)
         self._sync_weight_controls(weight, update_active=False)
@@ -852,7 +863,7 @@ class FontFormatPanel(Widget):
             self.verticalChecker.blockSignals(True)
             self.verticalChecker.setTristate(True)
             self.verticalChecker.setCheckState(Qt.CheckState.PartiallyChecked)
-            self.verticalChecker.setStyleSheet(mixed_checkbox_style())
+            set_checker_mixed_style(self.verticalChecker)
             self.verticalChecker.blockSignals(False)
         if 'alignment' in self.mixed_fields:
             self.alignBtnGroup.setMixed()
